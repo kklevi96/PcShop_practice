5b46426aacee5ee18f4cbd3c69a15aa7e6c37541
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-05-19 23:56:31 +0200

    added sample projects

---
+++ MintaZH/MintaZH/MintaZH/Logic/AthleteLogic.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using MintaZH.Models;
+using MintaZH.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH.Logic
+{
+    public class AthleteLogic : IAthleteLogic
+    {
+        IList<Athlete> athletes;
+        IList<Athlete> competetion;
+        private readonly IMessenger messenger;
+        private readonly IAthleteService athleteService;
+
+        public int AllAthlete { get { return competetion != null ? competetion.Count : 0; } }
+
+        public double AthleteSumMarketValue { get { return (competetion == null || competetion.Count == 0) ? 0 : competetion.Sum(x => x.ActualMarketValue); } }
+
+        public AthleteLogic(IMessenger messenger, IAthleteService athleteService)
+        {
+            this.messenger = messenger;
+            this.athleteService = athleteService;
+        }
+
+        public void AddAthelete(Athlete athlete)
+        {
+            if (!competetion.Contains(athlete))
+            {
+                competetion.Add(athlete);
+                messenger.Send("Athlete Added", "AthleteInfo");
+            }
+        }
+
+        public void EditAthlete(Athlete athlete)
+        {
+            //megnyitás, details lesz
+            athleteService.Edit(athlete);
+        }
+
+        public void RemoveFromCompetetion(Athlete athlete)
+        {
+            competetion.Remove(athlete);
+            messenger.Send("Athlete Removed", "AthleteInfo");
+        }
+
+        public void SaveCompetetion(string fileName, string when)
+        {
+            var competitionInJson = JsonConvert.SerializeObject(competetion);
+            File.WriteAllText($"{fileName}-{when}.json", competitionInJson);
+        }
+
+        public void LoadAthletes(IList<Athlete> athletes, IList<Athlete> competetion)
+        {
+            this.athletes = athletes;
+            this.competetion = competetion;
+
+            athletes.Add(new Athlete() { Name = "Attila", Event = "400m", PersonalBest = 48.90, SeasonBest = 49.51, IsValid = true });
+            athletes.Add(new Athlete() { Name = "Gergő", Event = "100m", PersonalBest = 48.90, SeasonBest = 49.51 });
+            athletes.Add(new Athlete() { Name = "Balázs", Event = "200m", PersonalBest = 48.90, SeasonBest = 49.51 });
+            athletes.Add(new Athlete() { Name = "Ottó", Event = "400m", PersonalBest = 48.90, SeasonBest = 49.51 });
+            athletes.Add(new Athlete() { Name = "Péter", Event = "400m", PersonalBest = 48.90, SeasonBest = 49.51 });
+            messenger.Send("Athletes Added", "AthleteInfo");
+        }
+    }
+}
---
+++ MintaZH/MintaZH/MintaZH/Logic/IAthleteLogic.cs
@@ -0,0 +1,17 @@
+﻿using MintaZH.Models;
+using System.Collections.Generic;
+
+namespace MintaZH.Logic
+{
+    public interface IAthleteLogic
+    {
+        int AllAthlete { get; }
+        double AthleteSumMarketValue { get; }
+
+        void AddAthelete(Athlete trooper);
+        void EditAthlete(Athlete trooper);
+        void RemoveFromCompetetion(Athlete trooper);
+        void SaveCompetetion(string fileName, string when);
+        void LoadAthletes(IList<Athlete> athletes, IList<Athlete> competetion);
+    }
+}
